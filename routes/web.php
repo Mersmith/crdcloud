86db0e523f41fee7bc03b2388c9a28cf8e255047
Mersmith <mersmith14@gmail.com>
2023-03-03 18:50:08 -0500

    21_Modificacion

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', AdministradorIngresarLivewire::class)->name('inicio');
 
-Route::get('registrar-odontologo', OdontologoRegistrarLivewire::class)->name('regitrar.odontologo');
+Route::get('registrar', OdontologoRegistrarLivewire::class)->name('regitrar.odontologo');
 
 
 Route::middleware([
